1d81a8f58f4aa9add1f8f7ae97e8d288b5d0be4c
JARJARBIN's studio <105795201+Jarjarbin06@users.noreply.github.com>
2026-06-11 18:21:17 +0200

    [FIX] invalid path for tests

--- sources/tests/JT_Flags/JT_Logs.py
+++ sources/tests/JT_Flags/JT_Logs.py
@@ -12,21 +12,21 @@ from jarbin_toolkit_jartest import JarTest, Get, Assertion
 # LOG FLAGS
 # =========================================================
 def JT_json_log():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--json-log")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--json-log")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
 
 
 def JT_verbose():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--verbose")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--verbose")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
 
 
 def JT_super_verbose():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--super-verbose")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--super-verbose")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
@@ -36,14 +36,14 @@ def JT_super_verbose():
 # LOG CONTROL FLAGS
 # =========================================================
 def JT_no_log():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--no-log")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--no-log")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
 
 
 def JT_show_log():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--show-log")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--show-log")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
@@ -53,21 +53,21 @@ def JT_show_log():
 # SILENT MODES
 # =========================================================
 def JT_silent():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--silent")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--silent")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
 
 
 def JT_super_silent():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--super-silent")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--super-silent")
 
     Assertion.eq(err, "")
     Assertion.neq(out, "")
 
 
 def JT_extreme_silent():
-    out, err, code = Get.Redirect.cmd_all_std(" ./bin/JCCS", "--extreme-silent")
+    out, err, code = Get.Redirect.cmd_all_std("python", "./sources/JCCS.py", "--extreme-silent")
 
     Assertion.eq(err, "")
     Assertion.eq(out, "")
